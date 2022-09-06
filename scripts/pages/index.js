426461591b9ba150f0083e59a2b5e3c3c93eaea3
jeremy koundour <j.koundour@gmail.com>
2022-09-06 14:44:23 +0200

    test resolution bug sur github page n°2

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,4 @@
-import {photographerFactory} from '../factories/photographer.js'
+import { photographerFactory } from "../factories/photographer.js"
 
 async function getPhotographers() {
     // Penser à remplacer par les données récupérées dans le json
